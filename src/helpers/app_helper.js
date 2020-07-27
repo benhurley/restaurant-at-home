7cf4c4b0fbc381749e3cbf8cfb9f52c85bd288c1
Benjamin Hurley <benfromtech@gmail.com>
2020-07-27 18:42:40 -0400

    remove extra chars from url regx

--- src/helpers/app_helper.js
+++ src/helpers/app_helper.js
@@ -4,7 +4,7 @@ export const updateUrl = (selectedRestaurant) => {
     if (selectedRestaurant) {
         if (window.location.search) {
           if (window.location.search.includes('id')){
-           newURL = window.location.href.replace(/(id=)[^\&]+/, '$1' + selectedRestaurant)
+           newURL = window.location.href.replace(/(id=)[^]+/, '$1' + selectedRestaurant)
           } else {
            newURL = window.location.href + `&id=${selectedRestaurant}`
           }
