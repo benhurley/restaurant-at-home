2975c6b09365becca92054f82effaf2f7005353d
Eric <Natelsoneric@gmail.com>
2020-07-29 17:53:39 -0400

    generic and specialized api's

--- api/api.py
+++ api/api.py
@@ -1,20 +1,15 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 from test_data.restaurant_data import RESTAURANTS
 
 app = Flask(__name__)
 
 @app.route('/api/restaurants')
-def get_restaurants():
-    results = []
+def get_all_restaurants():
+    return jsonify(list(RESTAURANTS))
 
-    if 'id' in request.args:
-        id = request.args['id']
-        for n in RESTAURANTS:
-            if str(n['id']) == id:
-                results.append(n)
+@app.route('/api/restaurants/<int:id>')
+def get_restaurants(id):
+    if id is not None:
+        return jsonify(list(filter(lambda restaurants, id=id: id == restaurants['id'], RESTAURANTS)))
     else:
-        for n in RESTAURANTS:
-            results.append(n)
-    
-    return {'restaurants': results}
+        return list(RESTAURANTS)
