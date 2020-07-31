0294e6e9400e4d8a2c5e83a3bcf8afebc4807cc4
Benjamin Hurley <benfromtech@gmail.com>
2020-07-30 23:01:35 -0400

    fix url paths in app.py

--- api/app.py
+++ api/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request
 from restaurant_data import RESTAURANTS
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='/build', static_url_path='')
 
 @app.route('/')
-def index():
+def root():
     return app.send_static_file('index.html')
 
 @app.route('/api/restaurants')
