f98a5b95ab35fe3cfd2bf13bbbf77a146cbcfcc6
Benjamin Hurley <benfromtech@gmail.com>
2020-07-30 20:47:12 -0400

    init commit for heroku

--- api/api.py
+++ api/api.py
@@ -2,7 +2,11 @@ from flask import Flask
 from flask import request
 from test_data.restaurant_data import RESTAURANTS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/api/restaurants')
 def get_restaurants():
@@ -18,3 +22,6 @@ def get_restaurants():
             results.append(n)
     
     return {'restaurants': results}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
