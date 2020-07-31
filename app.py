6a30a9f57de527ef5f4cad0f0b97a4453dd16f0f
Benjamin Hurley <benfromtech@gmail.com>
2020-07-31 00:15:52 -0400

    working heroku deployment

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-import os
-from flask import Flask, request, send_from_directory
+from flask import Flask, request
 from restaurant_data import RESTAURANTS
 
 app = Flask(__name__, static_folder='./build', static_url_path="/")
 
-# Serve React App
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
+def catch_all(path):
+  return app.send_static_file('index.html')
 
 @app.route('/api/restaurants')
 def get_restaurants():
