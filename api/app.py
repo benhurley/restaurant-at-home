1947c6d59dbd09c8c7eaa37d4e71ba0b738d3359
Benjamin Hurley <benfromtech@gmail.com>
2020-07-30 22:38:22 -0400

    move test data into app.py

--- api/app.py
+++ api/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-from test_data.restaurant_data import RESTAURANTS
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
@@ -22,3 +21,42 @@ def get_restaurants():
     
     return {'restaurants': results}
 
+RESTAURANTS = [
+    {
+        "id": 1,
+        "name": "Joe's Italian Restaurant",
+        "playlist": "Italian Cooking Music",
+        "lighting": "80%",
+        "scent": "vanilla candles",
+        "tableItems": [ "cloth napkins", "salt", "pepper", "chili peppers", "olive oil", "silverware" ],
+    },
+    {
+        "id": 2,
+        "name": "Jane's Ramen Spot",
+        "playlist": "Top 50 USA",
+        "lighting": "60%",
+        "scent": "palo santo sticks",
+        "tableItems": [ "chop sticks," "paper napkins", "cayenne", "sake" ],
+    },
+    {
+        "id": 3,
+        "name": "49 Club Steakhouse",
+        "playlist": "Frank Sinatra",
+        "lighting": "50%",
+        "tableItems": [ "silverware," "steak sauce", "salt", "pepper", "wine bottle holder", "toothpicks" ],
+    },
+    {
+        "id": 4,
+        "name": "ATK",
+        "playlist": "Deep Hau5",
+        "lighting": "40%",
+        "tableItems": [],
+    },
+    {
+        "id": 5,
+        "name": "Dumplings 2 Go",
+        "playlist": "Easy Listening",
+        "lighting": "20%",
+        "tableItems": ["soy sauce", "napkins"],
+    },
+]
