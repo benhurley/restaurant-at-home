241c720c390989e780befa665fac4f338bb1d238
Eric <Natelsoneric@gmail.com>
2020-08-01 12:08:09 -0400

    bulk post endpoint, still have a serialization issue

--- api/api.py
+++ api/api.py
@@ -5,7 +5,7 @@ from test_data.restaurant_data import RESTAURANTS
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://restaurantathome:<text me for password>@cluster0.536qw.mongodb.net/restaurantathome?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+app.config["MONGO_URI"] = "mongodb+srv://restaurantathome:omgpassword@cluster0.536qw.mongodb.net/restaurantathome?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
 mongo = PyMongo(app)
 
 # Custom encoder for MongoDB (ObjectId class isn't serializable)
@@ -15,7 +15,7 @@ class JSONEncoder(json.JSONEncoder):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
-@app.route('/api/restaurants')
+@app.route('/api/restaurants', methods=['GET'])
 def get_all_restaurants():
     res=[]
     collection = mongo.db.restaurants.find()
@@ -23,6 +23,33 @@ def get_all_restaurants():
         res.append(doc)
     return JSONEncoder().encode(res)
 
-@app.route('/api/restaurants/<int:id>')
+# Takes a list of restaurants to add to the database
+@app.route('/api/restaurants', methods=['POST'])
+def add_restaurants():
+    restaurants = mongo.db.restaurants
+    body = request.get_json()
+    to_insert = []
+    for item in body:
+        to_insert.append({
+            'name' : item.get('name'),
+            'playlist': item.get('playlist'),
+            'lighting': item.get('lighting'),
+            'scent': item.get('scent'),
+            'tableItems': item.get('tableItems')
+        })
+    print(jsonify(to_insert))
+    new_ids = restaurants.insert_many(to_insert)
+    # restaurants.delete_many(new_ids)
+    return JSONEncoder().encode(new_ids)
+    
+
+@app.route('/api/restaurants/<int:id>', methods = ['GET', 'POST', 'DELETE'])
 def get_restaurants(id):
-    return JSONEncoder().encode(mongo.db.restaurants.find_one({"id":id}))
+    if request.method == 'GET':
+        return JSONEncoder().encode(mongo.db.restaurants.find_one({"id":id}))
+
+    # if request.method == 'POST':
+
+    # if request.method == 'DELETE':
+    
+
